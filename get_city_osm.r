8c7bf766053e295d1de8b1dc929a3a568770d427
Pietro Colombo <p.colombo@campus.unimib.it>
2019-02-06 15:15:23 +0100

    ottimizazione codice

--- get_city_osm.r
+++ get_city_osm.r
@@ -5,29 +5,14 @@ if(!require(revgeo)){
 }
 
 perc_csv <- "../progetto_data_tech_&_machine_learning_dataset/dataset_with_add_features.csv"
+dati <- read.csv(perc_csv, header = TRUE, sep =",", quote = "\"", dec = ".")
 
 file.remove("dataset_with_osm.csv")
 
-dati$state <- ""
-dati$country <- ""
-dati$city <- ""
+info <- revgeo(longitude = dati$Longitude, latitude = dati$Latitude, output='frame')
 
-state <- dati$state
-country <- dati$country
-city <- dati$city
-
-for(i_row in 1:nrow(dati))
-{
-  info = evgeo(longitude = dati$Longitude[i_row], latitude = dati$Latitude[i_row], output='frame')
-  
-  state[i_row] <- as.character(info$state)
-  country[i_row] <- as.character(info$country)
-  city[i_row] <- as.character(info$city)
-
-}
-
-dati$state <- state
-dati$country <- country
-dati$city <- city
+dati$state <- as.character(info$state)
+dati$country <- as.character(info$country)
+dati$city <- as.character(info$city)
 
 write.csv(dati,file="dataset_with_osm_city.csv" ,row.names=FALSE) 
