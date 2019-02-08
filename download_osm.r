8891b2f752df69049da00029c25bf9bcb4fcef3a
Pietro Colombo <pietrocolombo@me.com>
2019-02-08 17:47:52 +0100

    change download osm

--- download_osm.r
+++ download_osm.r
@@ -9,36 +9,49 @@ dati <- read.csv(perc_csv, header = TRUE, sep =",", quote = "\"", dec = ".")
 
 perc <- "osm/"
 
-for(i_row in 2486:nrow(dati))
+cond <- c(TRUE, (dati$Id_user[-nrow(dati)] == dati$Id_user[-1]) & (dati$Id_perc[-nrow(dati)] == dati$Id_perc[-1]) & (dati$Label[-nrow(dati)] == dati$Label[-1]))
+
+index <- 0
+
+for(i_row in 1:nrow(dati))
 {
   if(i_row %% 10000 == 0)
   {
     print(i_row)
   }
-  url <- "http://overpass-api.de/api/interpreter?data=("
-  
-  area <- paste0("(around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row], ")")
-  
-  highwaysQuery <- paste0("node[highway]", area, ";",
-                          "way[highway]", area, ";",
-                          "relation[highway]", area, ";")
-  
-  aerowayQuery <- paste0("node[aeroway]", area, ";",
-                          "way[aeroway]", area, ";",
-                          "relation[aeroway]", area, ";")
-  
-  railwayQuery <- paste0("node[railway]", area, ";",
-                         "way[railway]", area, ";",
-                         "relation[railway]", area, ";")
-  
-   url <- paste0("http://overpass-api.de/api/interpreter?data=(way[","highway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");way[","aeroway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");way[","railway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");>;);out%20meta;")
-  
-  url <- paste0(url, highwaysQuery, aerowayQuery, railwayQuery, ";(._;>;);out%20meta;")
-  data <- tryCatch(getURLAsynchronous(url), error = function(e) "Error")
-  osm <-getURL(url)
-  
-  fileConn<-file(paste0(perc,i_row,".osm"))
-  writeLines(osm, fileConn)
-  close(fileConn)
+  if(cond)
+  {
+   index <- index + 1 
+  }
+  else
+  {
+    url <- "http://overpass-api.de/api/interpreter?data=("
+    
+    area <- paste0("(around:150,",dati$Latitude[i_row - (index%/%2)],",",dati$Longitude[i_row - (index%/%2)], ")")
+    
+    highwaysQuery <- paste0("node[highway]", area, ";",
+                            "way[highway]", area, ";",
+                            "relation[highway]", area, ";")
+    
+    aerowayQuery <- paste0("node[aeroway]", area, ";",
+                           "way[aeroway]", area, ";",
+                           "relation[aeroway]", area, ";")
+    
+    railwayQuery <- paste0("node[railway]", area, ";",
+                           "way[railway]", area, ";",
+                           "relation[railway]", area, ";")
+    
+    # url_old <- paste0("http://overpass-api.de/api/interpreter?data=(way[","highway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");way[","aeroway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");way[","railway","](around:100,",dati$Latitude[i_row],",",dati$Longitude[i_row],");>;);out%20meta;")
+    
+    url <- paste0(url, highwaysQuery, aerowayQuery, railwayQuery, ");(._;>;);out%20meta;")
+    osm <- tryCatch(getURLAsynchronous(url), error = function(e) "Error")
+    # osm <-getURL(url)
+    
+    fileConn<-file(paste0(perc, (i_row - (index%/%2)), ".osm"))
+    writeLines(osm, fileConn)
+    close(fileConn)
+    
+    index <- 0
+  }
 }
 
