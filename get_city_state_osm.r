b14c8afabb69f78769c46399cc3ab25fee7e288d
Pietro Colombo <pietrocolombo@me.com>
2019-02-10 11:02:50 +0100

    add BING

--- get_city_state_osm.r
+++ get_city_state_osm.r
@@ -1,12 +1,20 @@
 
+# query a open street map per ottenere le informazioni dello stato, city, regione 
+# sul punto di partenza e di arrivo di ogni percorso
+
 if(!require(revgeo)){
   install.packages("revgeo")
   library("revgeo")
 }
 
+not_use_BING = FALSE
+
+# leggo il csv generato dallo script compress_database.r
 perc_csv <- "dataset_compresso.csv"
 dati <- read.csv(perc_csv, header = TRUE, sep =",", quote = "\"", dec = ".")
 
+# istanzio i nuovi vettori per le nuove informazioni pre allocando la memoria
+# per ottimizare le performance
 stateStart <- vector(mode="character", length=nrow(dati))
 countryStart <- vector(mode="character", length=nrow(dati))
 cityStart <- vector(mode="character", length=nrow(dati))
@@ -22,10 +30,21 @@ for(i_row in 1:nrow(dati))
   {
     print(i_row)
   }
-  info_S <- revgeo(longitude = dati$longitudeStart[i_row], latitude = dati$latitudeStart[i_row], output='hash')
+  # query a open street map per ottenere le informazioni dello stato, city, regione del punto di partenza
+  if(not_use_BING)
+  {
+    info_S <- revgeo(longitude = dati$longitudeStart[i_row], latitude = dati$latitudeStart[i_row], output='hash')
+    info_E <- revgeo(longitude = dati$longitudeEnd[i_row], latitude = dati$latitudeEnd[i_row], output='hash')
+  }
+  else
+  {
+    info_S <- revgeo(longitude = dati$longitudeStart[i_row], latitude = dati$latitudeStart[i_row], provider = "bing", API = "AigJWwO_gPozxV3zLU9eG5oGKcH5VEEoIb5fp_58AT2QUWj6pNRvg5fitEz_mXT_", output='hash')
+    info_E <- revgeo(longitude = dati$longitudeEnd[i_row], latitude = dati$latitudeEnd[i_row], provider = "bing", API = "AigJWwO_gPozxV3zLU9eG5oGKcH5VEEoIb5fp_58AT2QUWj6pNRvg5fitEz_mXT_", output='hash')
+  }
   
-  info_E <- revgeo(longitude = dati$longitudeEnd[i_row], latitude = dati$latitudeEnd[i_row], output='hash')
+  # query a open street map per ottenere le informazioni dello stato, city, regione del punto di arrivo
   
+  # assegno i valori che mi arrivano da open street map
   stateStart[i_row] <- as.character(info_S$state)
   countryStart[i_row] <- as.character(info_S$country)
   cityStart[i_row] <- as.character(info_S$city)
@@ -34,6 +53,8 @@ for(i_row in 1:nrow(dati))
   countryEnd[i_row] <- as.character(info_E$country)
   cityEnd[i_row] <- as.character(info_E$city)
 }
+
+# aggiungo i nuovi vettori al dataframe iniziale
 dati$stateStart <- stateStart
 dati$countryStart <- countryStart
 dati$cityStart <- cityStart
@@ -42,6 +63,7 @@ dati$stateEnd <- stateEnd
 dati$countryEnd <- countryEnd
 dati$cityEnd <- cityEnd
 
+# salvo le informazio
 write.csv(dati,file="dataset_compresso_info_city_v2.csv" ,row.names=FALSE) 
 
   
