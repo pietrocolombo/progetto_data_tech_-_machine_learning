c9ecf2129bf3a8b9ca2c704d0c2d1cdd18d46423
carlo <c.radice@campus.unimib.it>
2019-02-11 22:29:42 +0100

    Added plots for ROC curve in ROC_computation_Bayes

--- ROC_computation_Bayes.R
+++ ROC_computation_Bayes.R
@@ -7,6 +7,10 @@ if(!require(ROCR)){
   install.packages("ROCR")
   library("ROCR")
 }
+if(!require(caret)){
+  install.packages("caret")
+  library("caret")
+}
 
 
 #Binarization of the classes
@@ -20,231 +24,99 @@ test_set$is_car <- as.numeric(test_set$target == 'car')
 training_set$is_bus <- as.numeric(training_set$target == 'bus')
 test_set$is_bus <- as.numeric(test_set$target == 'bus')
 
-training_set$is_airplane <- as.numeric(training_set$target == 'airplane')
-test_set$is_airplane <- as.numeric(test_set$target == 'airplane')
-
 training_set$is_bike <- as.numeric(training_set$target == 'bike')
 test_set$is_bike <- as.numeric(test_set$target == 'bike')
-
-training_set$is_boat<- as.numeric(training_set$target == 'boat')
-test_set$is_boat <- as.numeric(test_set$target == 'boat')
-
-training_set$is_subway <- as.numeric(training_set$target == 'subway')
-test_set$is_subway <- as.numeric(test_set$target == 'subway')
 
 training_set$is_train <- as.numeric(training_set$target == 'train')
 test_set$is_train <- as.numeric(test_set$target == 'train')
 
+training_set$is_walk <- as.factor(training_set$is_walk)
+training_set$is_car <- as.factor(training_set$is_car)
+training_set$is_bus <- as.factor(training_set$is_bus)
+training_set$is_train <- as.factor(training_set$is_train)
+training_set$is_bike <- as.factor(training_set$is_bike)
 
-library(e1071)
+test_set$is_walk <- as.factor(test_set$is_walk)
+test_set$is_car <- as.factor(test_set$is_car)
+test_set$is_bus <- as.factor(test_set$is_bus)
+test_set$is_train <- as.factor(test_set$is_train)
+test_set$is_bike <- as.factor(test_set$is_bike)
+################### NAIVE BAYES ROC EVALUATION FOR EACH CLASS #############################
 
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
+# Evaluating ROC for each class
+mod_walk = train(is_walk ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + city_changed + type,
+                 data = training_set,
+                 preProcess= c("center","scale"),
+                 method = 'nb',
+                 trControl=trctrl)
 
-################### SVM RADIAL KERNEL ROC EVALUATION FOR EACH CLASS#############################
+mod_car = train(is_car ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + city_changed + type,
+                data = training_set,
+                preProcess= c("center","scale"),
+                method = 'nb',
+                trControl=trctrl)
 
-# Evaluating models for each class
-mod_walk = svm(is_walk ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set,
-               method = "C-classification",
-               kernal = "radial",
-               preProcess = c("center","scale"), #scaling values for svm
-               trControl=trctrl,
-               gamma = 0.5,
-               cost = 5)
+mod_bus = train(is_bus ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + city_changed + type,
+                data = training_set,
+                preProcess= c("center","scale"),
+                method = 'nb',
+                trControl=trctrl)
 
-mod_car = svm(is_car ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set,
-              method = "C-classification",
-              kernal = "radial",
-              preProcess = c("center","scale"), #scaling values for svm
-              trControl=trctrl,
-              gamma = 0.5,
-              cost = 5)
+mod_bike = train(is_bike ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + city_changed + type,
+                 data = training_set,
+                 preProcess= c("center","scale"),
+                 method = 'nb',
+                 trControl=trctrl)
 
-mod_bus = svm(is_bus ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set,
-              method = "C-classification",
-              kernal = "radial",
-              preProcess = c("center","scale"), #scaling values for svm
-              trControl=trctrl,
-              gamma = 0.5,
-              cost = 5)
-
-mod_airplane = svm(is_airplane ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set, 
-                   method = "C-classification",
-                   kernal = "radial",
-                   preProcess = c("center","scale"), #scaling values for svm
-                   trControl=trctrl,
-                   gamma = 0.5,
-                   cost = 5)
-
-mod_bike = svm(is_bike ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set,
-               method = "C-classification",
-               kernal = "radial",
-               preProcess = c("center","scale"), #scaling values for svm
-               trControl=trctrl,
-               gamma = 0.5,
-               cost = 5)
-
-mod_boat = svm(is_boat ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set, 
-                 method = "C-classification",
-                 kernal = "radial",
-                 preProcess = c("center","scale"), #scaling values for svm
-                 trControl=trctrl,
-                 gamma = 0.5,
-                 cost = 5)
-
-mod_subway = svm(is_subway ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set, 
-                 method = "C-classification",
-                 kernal = "radial",
-                 preProcess = c("center","scale"), #scaling values for svm
-                 trControl=trctrl,
-                 gamma = 0.5,
-                 cost = 5)
-
-mod_train = svm(is_train ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + type , data=training_set, 
-                method = "C-classification",
-                kernal = "radial",
-                preProcess = c("center","scale"), #scaling values for svm
-                trControl=trctrl,
-                gamma = 0.5,
-                cost = 5)
-
-
-pred_walk <- prediction(predict(mod_walk, test_set), test_set$is_walk)
-pred_car <- prediction(predict(mod_car, test_set), test_set$is_car)
-pred_bus <- prediction(predict(mod_bus, test_set), test_set$is_bus)
-pred_airplane <- prediction(predict(mod_airplane, test_set), test_set$is_airplane)
-pred_bike <- prediction(predict(mod_bike, test_set), test_set$is_bike)
-pred_boat <- prediction(predict(mod_boat, test_set), test_set$is_boat)
-pred_subway <- prediction(predict(mod_subway, test_set), test_set$is_subway)
-pred_train <- prediction(predict(mod_train, test_set), test_set$is_train)
-
-
-perf_walk <- performance(pred_walk,"tpr","fpr")
-perf_car <- performance(pred_car,"tpr","fpr")
-perf_bus <- performance(pred_bus,"tpr","fpr")
-perf_airplane <- performance(pred_airplane,"tpr","fpr")
-perf_bike <- performance(pred_bike,"tpr","fpr")
-perf_boat <- performance(pred_boat,"tpr","fpr")
-perf_subway <- performance(pred_subway,"tpr","fpr")
-perf_train <- performance(pred_train,"tpr","fpr")
+mod_train = train(is_train ~ vcr + sr + hcr + vel_max + vel_avg + altitude_max + altitude_avg + tot_duration + tot_distance + state_changed + city_changed + type,
+                  data = training_set,
+                  preProcess= c("center","scale"),
+                  method = 'nb',
+                  trControl=trctrl)
 
 
 
-plot(perf_walk, col= "black")
+training_set$is_walk <- as.numeric(training_set$is_walk)
+training_set$is_car <- as.numeric(training_set$is_car)
+training_set$is_bus <- as.numeric(training_set$is_bus)
+training_set$is_train <- as.numeric(training_set$is_train)
+training_set$is_bike <- as.numeric(training_set$is_bike)
+
+# ROC CURVEs
+
+predict_walk <- predict(mod_walk, test_set)
+roc_obj_walk <- roc(test_set$is_walk, as.numeric(predict_walk))
+print(auc(roc_obj_walk))
+rs <- roc_obj_walk[['rocs']]
+plot(ggroc(roc_obj_walk, legacy.axes=TRUE) + geom_abline(slope=1, intercept=0, color="red", linetype=3) + theme_bw())
+
 par(new=TRUE)
-plot(perf_car, col = "red")
+predict_car <- predict(mod_car, test_set)
+roc_obj_car <- roc(test_set$is_car, as.numeric(predict_car))
+print(auc(roc_obj_car))
+rs <- roc_obj_car[['rocs']]
+plot(ggroc(roc_obj_car, legacy.axes=TRUE) + geom_abline(slope=1, intercept=0, color="red", linetype=3) + theme_bw())
+
 par(new=TRUE)
-plot(perf_bus, col = "yellow")
+predict_bus <- predict(mod_bus, test_set)
+roc_obj_bus <- roc(test_set$is_bus, as.numeric(predict_bus))
+print(auc(roc_obj_bus))
+rs <- roc_obj_bus[['rocs']]
+plot(ggroc(roc_obj_bus, legacy.axes=TRUE) + geom_abline(slope=1, intercept=0, color="red", linetype=3) + theme_bw())
+
 par(new=TRUE)
-plot(perf_airplane, col = "green")
+predict_train <- predict(mod_train, test_set)
+roc_obj_train <- roc(test_set$is_train, as.numeric(predict_train))
+print(auc(roc_obj_train))
+rs <- roc_obj_train[['rocs']]
+plot(ggroc(roc_obj_train, legacy.axes=TRUE) + geom_abline(slope=1, intercept=0, color="red", linetype=3) + theme_bw())
+
 par(new=TRUE)
-plot(perf_bike, col = "blue")
-par(new=TRUE)
-plot(perf_boat, col = "light blue")
-par(new=TRUE)
-plot(perf_subway, col = "orange")
-par(new=TRUE)
-plot(perf_train, col = "violet")
-par(new=TRUE)
-abline(0,1, col ="black")
+predict_bike <- predict(mod_bike, test_set)
+roc_obj_bike <- roc(test_set$is_bike, as.numeric(predict_bike))
+print(auc(roc_obj_bike))
+rs <- roc_obj_bike[['rocs']]
+plot(ggroc(roc_obj_bike, legacy.axes=TRUE) + geom_abline(slope=1, intercept=0, color="red", linetype=3) + theme_bw())
 
 
-legend(0.55, 0.5, c("walk", "car","bus","airplane","bike","boat","subway","train"), 
-      lty=1, col=c("black","red","yellow","green","blue","light blue","orange","violet"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM")
-
-#calcolo di AUC for each class 
-pred_walk <- predict(mod_walk, test_set)
-roc_obj <- roc(test_set$is_walk, pred_walk)
-au_walk = auc(roc_obj)
-print(au_walk)
-
-plot(perf_walk, col= "black")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("walk"), 
-       lty=1, col=c("black"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_walk ))
-
-pred_car <- predict(mod_car, test_set)
-roc_obj <- roc(test_set$is_car, pred_car)
-au_car = auc(roc_obj)
-print(au_car)
-
-plot(perf_car, col = "red")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("car"), 
-       lty=1, col=c("red"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_car ))
-
-pred_bus <- predict(mod_bus, test_set)
-roc_obj <- roc(test_set$is_bus, pred_bus)
-au_bus = auc(roc_obj)
-print(au_bus)
-
-plot(perf_bus, col = "yellow")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("bus"), 
-       lty=1, col=c("yellow"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_bus ))
-
-pred_airplane <- predict(mod_airplane, test_set)
-roc_obj <- roc(test_set$is_airplane, pred_airplane)
-au_airplane = auc(roc_obj)
-print(au_airplane)
-
-plot(perf_airplane, col = "green")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("airplane"), 
-       lty=1, col=c("green"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_airplane ))
-
-pred_bike <- predict(mod_bike, test_set)
-roc_obj <- roc(test_set$is_bike, pred_bike)
-au_bike = auc(roc_obj)
-print(au_bike)
-
-plot(perf_bike, col = "blue")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("bike"), 
-       lty=1, col=c("blue"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_bike ))
-
-pred_subway <- predict(mod_subway, test_set)
-roc_obj <- roc(test_set$is_subway, pred_subway)
-au_subway = auc(roc_obj)
-print(au_subway)
-
-plot(perf_subway, col = "orange")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("subway"), 
-       lty=1, col=c("orange"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_subway ))
-
-pred_train <- predict(mod_train, test_set)
-roc_obj <- roc(test_set$is_train, pred_train)
-au_train = auc(roc_obj)
-print(au_train)
-
-plot(perf_train, col = "violet")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("train"), 
-       lty=1, col=c("violet"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_train ))
-
-
-pred_boat <- predict(mod_boat, test_set)
-roc_obj <- roc(test_set$is_train, pred_boat)
-au_boat = auc(roc_obj)
-print(au_train)
-
-plot(perf_boat, col = "light blue")
-par(new=TRUE)
-abline(0,1, col ="black")
-legend(0.55, 0.5, c("boat"), 
-       lty=1, col=c("light blue"), bty='n', cex=1.0)
-title(main = "ROC Curve SVM", sub = paste0("Area under the curve: ", au_boat ))
